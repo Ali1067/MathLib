8283ee0dcbd251956175c76cc9ea4dded3195bef
muhammad.ali@ikslogics.com <muhammad.ali@ikslogics.com>
2023-07-10 13:56:19 +0500

    Initial Commit

---
+++ MathLib/MathLib.h
@@ -0,0 +1,18 @@
+//
+//  MathLib.h
+//  MathLib
+//
+//  Created by mac on 10/07/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MathLib.
+FOUNDATION_EXPORT double MathLibVersionNumber;
+
+//! Project version string for MathLib.
+FOUNDATION_EXPORT const unsigned char MathLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MathLib/PublicHeader.h>
+
+
